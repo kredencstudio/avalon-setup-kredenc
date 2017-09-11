850d0bc7b9c114cf2e197313a79c910e197d5fac
aardschok <wikoreman@gmail.com>
2017-09-11 12:41:35 +0200

    removed keys

--- avalon.bat
+++ avalon.bat
@@ -1,10 +1,9 @@
 @echo off
 
-set AVALON_CORE=P:\pipeline\2.1_dev\git\avalon-core
+set AVALON_CORE=C:\Users\User\Documents\development\avalon-core
 set AVALON_PROJECTS=P:\projects
 set AVALON_CONFIG=colorbleed
 set AVALON_LABEL=Avalon
-set AVALON_SENTRY=https://905a90e77a4942fa9619bdfd6ff7908b:ec09580529374359b67cc1c44a57a064@logs.mindbender.com/3
 set AVALON_MONGO=mongodb://STORAGE1:27017
 set AVALON_EARLY_ADOPTER=1
 
