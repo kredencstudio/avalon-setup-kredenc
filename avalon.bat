a51d04695e5ffa34016f7d8bec8964c15e3f301c
aardschok <wikoreman@gmail.com>
2017-10-03 11:00:34 +0200

    removed all environment settings

--- avalon.bat
+++ avalon.bat
@@ -1,14 +1,4 @@
 @echo off
 
-set AVALON_CORE=C:\Users\User\Documents\development\avalon-core
-set AVALON_PROJECTS=P:\projects
-set AVALON_CONFIG=colorbleed
-set AVALON_LABEL=Avalon
-set AVALON_MONGO=mongodb://STORAGE1:27017
-set AVALON_EARLY_ADOPTER=1
-
-set PATH=P:\pipeline\2.1_dev\bin\windows\python36;%PATH%
-set PYTHONPATH=P:\pipeline\2.1_dev\git\colorbleed-config;%PYTHONPATH%
-
 cd ..
 python %~dp0avalon.py %*
